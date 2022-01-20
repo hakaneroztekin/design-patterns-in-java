3720e41d92c0ced18b5eabd6213f984d3bd7156e
Hakan Eröztekin <h.eroztekin@gmail.com>
2022-01-20 07:27:16 +0300

    init spring project

---
+++ src/main/java/com/hakaneroztekin/designpatternsinjava/DesignPatternsInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hakaneroztekin.designpatternsinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsInJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hakaneroztekin/designpatternsinjava/DesignPatternsInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakaneroztekin.designpatternsinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsInJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
